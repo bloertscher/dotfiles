2c43b1846f07cfc48ea65e62ff69cfa8e3b65e6a
bloertscher <loertscher.b@outlook.com>
2019-07-01 18:31:33 -0600

    add cargo bin to bootstrap

--- emacs.d/lisp/personal/custom.el
+++ emacs.d/lisp/personal/custom.el
@@ -8,7 +8,8 @@
     ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(frame-background-mode (quote dark))
  '(inhibit-startup-screen t)
- '(initial-buffer-choice nil))
+ '(initial-buffer-choice nil)
+ '(vc-follow-symlinks nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,2 +1,3 @@
 path_prepend "$HOME/software/bin"
 path_prepend "$HOME/.dotfiles/bin"
+path_prepend "$HOME/.cargo/bin"
