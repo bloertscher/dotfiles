45dac8fb7188dd8bcfeb0cbb81005351f8e345fb
bloertscher <loertscher.b@outlook.com>
2020-07-04 22:16:43 -0600

    Remove emacs dir

--- emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-(add-to-list 'load-path "~/.emacs.d/lisp")
-
-(setq custom-file "~/.emacs.d/lisp/personal/custom.el")
-(load "personal/custom")
-(load "personal/theme")
-
-(let ((custom "~/.emacs_local"))
-  (when (file-exists-p custom)
-    (load-file custom)))
