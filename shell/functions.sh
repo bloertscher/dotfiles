fa0d797cf9b9104f7495340c28e0c7babfdde213
bloertscher <loertscher.b@outlook.com>
2020-09-25 20:07:57 -0600

    path munging

--- shell/functions.sh
+++ shell/functions.sh
@@ -12,3 +12,33 @@ path_prepend() {
     path_remove "$1"
     PATH="$1${PATH:+":$PATH"}"
 }
+
+lib_remove() {
+    LD_LIBRARY_PATH=$(echo -n $LD_LIBRARY_PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//')
+    # only output non-duplicates remove trailing ':'
+}
+
+lib_append() {
+    lib_remove "$1"
+    LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}$1"
+}
+
+lib_prepend() {
+    lib_remove "$1"
+    LD_LIBRARY_PATH="$1${LD_LIBRARY_PATH:+":$LD_LIBRARY_PATH"}"
+}
+
+man_remove() {
+    MANPATH=$(echo -n $MANPATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//')
+    # only output non-duplicates remove trailing ':'
+}
+
+man_append() {
+    man_remove "$1"
+    MANPATH="${MANPATH:+"$MANPATH:"}$1"
+}
+
+man_prepend() {
+    man_remove "$1"
+    MANPATH="$1${MANPATH:+":$MANPATH"}"
+}
